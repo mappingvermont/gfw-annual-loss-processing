bf196fd20e6b52314ce54e1039b9a7dbfc741871
Ubuntu <charlie.hofmann@gmail.com>
2017-03-21 15:50:50 -0400

    build out loss by thresh and year tables

--- loss_by_thresh_iso.py
+++ loss_by_thresh_iso.py
@@ -19,27 +19,27 @@ def main():
     else:
         writer = pd.ExcelWriter(output_excel)
     
-    extent_sql = 'SELECT iso, adm1, thresh, year, sum(area) as area FROM loss ' \
-                 'GROUP BY iso, adm1, thresh, year'
+    extent_sql = 'SELECT iso, thresh, year, sum(area) as area FROM loss ' \
+                 'GROUP BY iso, thresh, year'
 
     df = pd.read_sql(extent_sql, conn)
     df = add_lookup(df)
 
     # Create country field name
-    df['Country'] = df.name0 + '_' + df.name1
+    df['Country'] = df.name0
 
     # remove partial country data
     partial_list = ['AFG', 'ARE', 'CHN', 'ESP', 'IRN', 'IRQ', 'ISR', 'ITA', 'JOR', 'JPN',
                  'KGZ', 'KWT', 'OMN', 'PSE', 'SAU', 'TJK', 'TKM', 'USA', 'ALB', 'CYP',
                  'GRC', 'LBN', 'MUS', 'PRK', 'XNC', 'PRT', 'KOR', 'SYR', 'TUR', 'UZB']
 
-    complete_miss_list = ['ALA', 'ASM', 'AND', 'ATA', 'ARM', 'AUT', 'AZE', 'BLR', 'BEL', 'BMU',
-                         'BIH', 'BVT', 'IOT', 'BGR', 'CAN', 'XCA', 'CXR', 'CCK', 'COK', 'HRV',
-                         'CZE', 'DNK', 'EST', 'FRO', 'FIN', 'FRA', 'PYF', 'GEO', 'DEU', 'GRL',
-                         'GUM', 'GGY', 'HMD', 'HUN', 'ISL', 'IRL', 'IMN', 'JEY', 'KAZ', 'XKO',
-                         'LVA', 'LIE', 'LTU', 'LUX', 'MKD', 'MHL', 'MDA', 'MCO', 'MNG', 'MNE',
-                         'NLD', 'NIU', 'MNP', 'NOR', 'PCN', 'POL', 'ROU', 'RUS', 'SHN', 'SPM',
-                         'WSM', 'SMR', 'SRB', 'SVK', 'SVN', 'SGS', 'SJM', 'SWE', 'CHE', 'TKL',
+    complete_miss_list = ['ALA', 'ASM', 'AND', 'ATA', 'ARM', 'AUT', 'AZE', 'BLR', 'BEL', 'BMU', 
+                         'BIH', 'BVT', 'IOT', 'BGR', 'CAN', 'XCA', 'CXR', 'CCK', 'COK', 'HRV', 
+                         'CZE', 'DNK', 'EST', 'FRO', 'FIN', 'FRA', 'PYF', 'GEO', 'DEU', 'GRL', 
+                         'GUM', 'GGY', 'HMD', 'HUN', 'ISL', 'IRL', 'IMN', 'JEY', 'KAZ', 'XKO', 
+                         'LVA', 'LIE', 'LTU', 'LUX', 'MKD', 'MHL', 'MDA', 'MCO', 'MNG', 'MNE', 
+                         'NLD', 'NIU', 'MNP', 'NOR', 'PCN', 'POL', 'ROU', 'RUS', 'SHN', 'SPM', 
+                         'WSM', 'SMR', 'SRB', 'SVK', 'SVN', 'SGS', 'SJM', 'SWE', 'CHE', 'TKL', 
                          'TON', 'UKR', 'GBR', 'VAT', 'WLF']
 
     skip_list = partial_list + complete_miss_list
@@ -49,6 +49,8 @@ def main():
 
     thresh10_df = df[df.thresh == 10]
     thresh10_pivot = thresh10_df.pivot(index='Country', columns='year', values='area')
+
+    print thresh10_pivot.columns
 
     thresh10_pivot.loc[(thresh10_pivot[2015] != -9999), 'TOTAL 2001-2015'] = thresh10_pivot.sum(axis=1)
     thresh10_pivot.loc[(thresh10_pivot[2015] == -9999), 'TOTAL 2001-2015'] = -9999
@@ -62,7 +64,7 @@ def main():
 
         thresh10_pivot = pd.concat([thresh10_pivot, df_pivot], axis=1, join_axes=[thresh10_pivot.index])
 
-    thresh10_pivot.to_excel(writer, 'Loss (2001-2014) by Subnat')
+    thresh10_pivot.to_excel(writer, 'Loss (2001-2015) by Country')
 
     writer.save()
 
@@ -70,13 +72,13 @@ def main():
 def add_lookup(data_df):
 
     lkp_df = pd.read_sql('SELECT iso, adm1, adm2, name0, name1, name2 FROM adm_lkp', conn)
-    lkp_df = lkp_df.groupby(['iso', 'adm1', 'name0', 'name1']).size().reset_index()
+    lkp_df = lkp_df.groupby(['iso', 'name0']).size().reset_index()
 
     print 'got lkp df'
 
     del lkp_df[0]
 
-    data_df = pd.merge(data_df, lkp_df, on=['iso', 'adm1'], how='left')
+    data_df = pd.merge(data_df, lkp_df, on=['iso'], how='left')
 
     return data_df
     
