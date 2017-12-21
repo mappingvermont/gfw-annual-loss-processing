05a7c2a71cf20653f45bd733cef3df3aaae8e7ce
mappingvermont <charlie.hofmann@gmail.com>
2017-12-21 14:29:11 -0500

    add 2010 extent data to final output table, improve loss/gain/extent QC by using s3/lambda API

--- 1b_Vector-to-TSV/qc-output.py
+++ 1b_Vector-to-TSV/qc-output.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import subprocess
+import json
+import requests
 
 from rasterstats import zonal_stats
 import pandas as pd
@@ -13,7 +15,7 @@ def main():
     parser = argparse.ArgumentParser(description='QC loss and extent stats')
     parser.add_argument('--number-of-tiles', '-n', help='number of tiles to run QC', default=10, type=int, required=True)
     parser.add_argument('--grid-resolution', '-g', help='grid resolution of source', type=int, default=10, choices=(10, 0.25), required=True)
-    
+
     parser.add_argument('--s3-poly-dir', '-s', help='input poly directory for intersected TSVs', required=True)
     parser.add_argument('--output-dataset-id', '-o', help='the output dataset in the API to compare results', required=True)
 
@@ -46,9 +48,9 @@ def qc_output_tile(tile_name, s3_src_dir, temp_dir):
 
 
 def filter_valid_adm2_boundaries(zstats_df, tile_name):
-    
+
     tile_id = os.path.splitext(tile_name)[0].split('__')[-1]
-    
+
     grid_df = gpd.read_file(os.path.join('grid', 'lossdata_footprint_filter.geojson'))
     grid_df = grid_df[grid_df.ID == tile_id].reset_index()
     bounds = grid_df.ix[0].geometry.bounds
@@ -57,18 +59,22 @@ def filter_valid_adm2_boundaries(zstats_df, tile_name):
 
     sql = ('SELECT iso, id_1, id_2 '
            'FROM adm2_final '
-           'WHERE ST_Contains(ST_MakeEnvelope( ' 
+           'WHERE ST_Contains(ST_MakeEnvelope( '
            '{}, {}, {}, {}, 4326), geom) '
            'GROUP BY iso, id_1, id_2 ').format(*bounds)
-    
+
     adm2_df = pd.read_sql_query(sql, con=engine)
+
+    zstats_df.id_1 = zstats_df.id_1.astype(int)
+    zstats_df.id_2 = zstats_df.id_2.astype(int)
 
     return pd.merge(zstats_df, adm2_df, on=['iso', 'id_1', 'id_2'])
 
 
 def compare_to_api(df):
-    
-    polyname = df.field_2.unique()[0]
+    print df.head()
+
+    polyname = df.polyname.unique()[0]
     iso_str = "', '".join(df.iso.unique())
     id_1_str = ', '.join(df.id_1.unique().astype(str))
     id_2_str = ', '.join(df.id_2.unique().astype(str))
@@ -77,10 +83,10 @@ def compare_to_api(df):
     # input polyname to polynames used here:
     # https://production-api.globalforestwatch.org/v1/query/499682b1-3174-493f-ba1a-368b4636708e?sql=SELECT%20polyname,%20count(*)%20FROM%20data%20GROUP%20BY%20polyname
 
-    sql = ("SELECT * FROM data WHERE " 
+    sql = ("SELECT * FROM data WHERE "
            "polyname = '{}' AND "
            "thresh = 30 AND "
-           "iso in ('{}') AND adm1 in ({}) " 
+           "iso in ('{}') AND adm1 in ({}) "
            "AND adm2 in ({}) ").format(
             polyname, iso_str, id_1_str, id_2_str)
 
@@ -90,37 +96,107 @@ def compare_to_api(df):
     # load into DF + unpack nested year values
     # compare to df
     # write to database or s3?
-    
+
 
 
 def convert_to_geojson(tile_name, s3_src_dir, temp_dir):
-    
+
     # copy down chosen tile to the temp directory
     s3_path = '{}{}'.format(s3_src_dir, tile_name)
-    cmd = ['aws', 's3', 'cp', s3_path, temp_dir] 
+    cmd = ['aws', 's3', 'cp', s3_path, temp_dir]
     subprocess.check_call(cmd)
 
     # write the VRT
     local_tsv = os.path.join(temp_dir, tile_name)
     local_vrt = os.path.join(temp_dir, tile_name.replace('.tsv', '.vrt'))
-    decode_tsv.build_vrt(local_tsv, local_vrt) 
-    
+    decode_tsv.build_vrt(local_tsv, local_vrt)
+
     # convert VRT to geojson
     local_geojson = os.path.join(temp_dir, tile_name.replace('.tsv', '.geojson'))
 
     cmd = ['ogr2ogr', '-f', 'GeoJSON', local_geojson, local_vrt]
     subprocess.check_call(cmd)
 
+    # open in geopandas
+    df = gpd.read_file(local_geojson)
+
+    # dissolve by attributes to make queries to the API easier
+    del df['field_1']
+    dissolve_fields = list(df.columns)[0:-1]
+
+    dissolved = df.dissolve(by=dissolve_fields).reset_index()
+
+    # give columns their proper names
+    dissolved.columns = ['polyname', 'bound1', 'bound2', 'bound3', 'bound4',
+                         'iso', 'id_1', 'id_2', 'geometry']
+
+
+    print dissolved.head()
+
+    # gpd can't overwrite, need to delete file first
+    os.remove(local_geojson)
+    dissolved.to_file(local_geojson, driver='GeoJSON')
+
     return local_geojson
+
 
 
 def calc_zstats(local_geojson):
 
-    # run the rasterstats process, returing the geojson object with stats 
+    resp_list = []
+
+    with open(local_geojson) as thefile:
+        data = json.load(thefile)
+
+    url = 'https://0yvx7602sb.execute-api.us-east-1.amazonaws.com/dev/umd-loss-gain'
+
+    for feat in data['features']:
+        print feat['properties']
+
+        payload = {'geojson': {'features': [feat]}}
+        params = {'aggregate_values': False}
+
+        r = requests.post(url, json=payload, params=params)
+        resp = r.json()
+
+        valid_zstats = False
+
+        try:
+            data = resp['data']['attributes']
+            valid_zstats = True
+        except KeyError:
+            print resp
+
+        if valid_zstats:
+            print data
+
+            for loss_year, loss_val in data['loss'].iteritems():
+                resp_dict = {'year': loss_year, 'area_loss': loss_val, 'area_gain': data['gain'],
+                             'area_extent': data['treeExtent'], 'poly_aoi_area': data['areaHa']}
+
+                row = merge_two_dicts(feat['properties'], resp_dict)
+                resp_list.append(row)
+        break
+
+    return pd.DataFrame(resp_list)
+
+
+
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+
+    return z
+
+
+
+def calc_zstats_rasterio(local_geojson):
+
+    # run the rasterstats process, returing the geojson object with stats
     loss_vrt = r's3://gfw2-data/forest_change/hansen_2016_masked_30tcd/data.vrt'
     kwargs = {'categorical': True, 'geojson_out': True, 'prefix': '_'}
-    geojson_with_stats = zonal_stats(local_geojson, loss_vrt, **kwargs) 
-    
+    geojson_with_stats = zonal_stats(local_geojson, loss_vrt, **kwargs)
+
     # convert to GDF
     df = gpd.GeoDataFrame.from_features(geojson_with_stats)
 
@@ -133,17 +209,17 @@ def calc_zstats(local_geojson):
     unique_cols = list([x for x in df.columns if x[0] != '_' and x != 'geometry'])
 
     df = pd.wide_to_long(df, '_', unique_cols, 'year').reset_index()
-    
+
     df.year = df.year.astype(int) + 2000
     df = df.rename(columns={'_': 'pixel_count', 'field_7': 'iso', 'field_8': 'id_1', 'field_9': 'id_2'})
 
     df.id_1 = df.id_1.astype(int)
-    df.id_2 = df.id_2.astype(int) 
+    df.id_2 = df.id_2.astype(int)
 
     df['loss_ha'] = df.pixel_area * df.pixel_count
 
     return df
-    
+
 
 if __name__ == '__main__':
     main()
--- 2_Cumulate-Results-and-Create-API-Datasets/postprocess.py
+++ 2_Cumulate-Results-and-Create-API-Datasets/postprocess.py
@@ -10,7 +10,8 @@ def main():
     # Parse commandline arguments
     parser = argparse.ArgumentParser(description='Clean up polynames, join loss and extent data')
     parser.add_argument('--loss-dataset', '-l', required=True, help='path to cum-summed loss csv')
-    parser.add_argument('--extent-dataset', '-e', required=True, help='path to cum-summed extent csv')
+    parser.add_argument('--extent-2000-dataset', '-e2000', required=True, help='path to cum-summed extent 2000 csv')
+    parser.add_argument('--extent-2010-dataset', '-e2010', required=True, help='path to cum-summed extent 2010 csv')
     parser.add_argument('--gain-dataset', '-g', required=True, help='path to gain dataset')
 
     parser.add_argument('--adm2-area-dataset', '-a', required=True, help='path to CSV of adm2 areas, grouped by iso/adm1/adm2') 
@@ -19,20 +20,31 @@ def main():
     args = parser.parse_args()
 
     loss_df = read_df(args.loss_dataset)
-    extent_df = read_df(args.extent_dataset)
+    extent_2000_df = read_df(args.extent_2000_dataset)
+    extent_2010_df = read_df(args.extent_2010_dataset)
     gain_df = read_df(args.gain_dataset)
 
     poly_aoi_df = read_df(args.polygon_dataset)
     adm2_area_df = pd.read_csv(args.adm2_area_dataset)
 
-    extent_with_area = add_area_to_extent_df(extent_df, adm2_area_df, poly_aoi_df, gain_df)  
+    extent_with_area = add_area_to_extent_df(extent_2000_df, extent_2010_df, adm2_area_df, poly_aoi_df, gain_df)
 
     field_list = ['polyname', 'bound1', 'bound2', 'bound3', 'bound4', 'thresh'] 
     
     merged = join_loss_extent(loss_df, extent_with_area, field_list)
 
+    qc_loss_extent(merged)
+
     for adm_level in range(0, 3):
         write_output(merged, adm_level, field_list)
+
+
+def qc_loss_extent(df):
+
+    if not df[(df.area_gadm28 < 0) | (df.area_gain < 0) | (df.area_extent < 0) | 
+              (df.area_poly_aoi < 0) | (df.area_extent_2000 < 0) | (df.area_loss < 0) | 
+              (df.emissions < 0) | (df.year < 0) ].empty:
+        raise ValueError('Final dataframe has negative values in area fields')
 
 
 def write_output(df, adm_level, field_list):
@@ -40,7 +52,7 @@ def write_output(df, adm_level, field_list):
     # build field list for grouping
     adm_list = ['iso', 'adm1', 'adm2'][0: adm_level + 1]
 
-    area_fields = ['area_extent', 'area_gadm28', 'area_poly_aoi', 'area_gain', 'area_loss', 'emissions']
+    area_fields = ['area_extent', 'area_gadm28', 'area_poly_aoi', 'area_gain', 'area_extent_2000', 'area_loss', 'emissions']
     group_list = field_list + adm_list
 
     print 'grouping data by adm level {} and polygon'.format(adm_level)
@@ -50,7 +62,7 @@ def write_output(df, adm_level, field_list):
     grouped = (df.groupby(group_list + ['year'])[area_fields]
                  .sum()
                  .reset_index()
-                 .groupby(group_list + area_fields[0:4], as_index=False)
+                 .groupby(group_list + area_fields[0:5], as_index=False)
                  .apply(lambda x: x[['year', 'area_loss', 'emissions']].to_dict('r'))
                  .reset_index()
                  .rename(columns={0: 'year_data'}))
@@ -66,21 +78,29 @@ def write_output(df, adm_level, field_list):
         thefile.write(json.dumps(as_dict, ensure_ascii=False))
 
 
-def add_area_to_extent_df(extent_df, adm2_area_df, poly_area_df, gain_df):
+def add_area_to_extent_df(extent_2000_df, extent_2010_df, adm2_area_df, poly_area_df, gain_df):
     
+    # join extent2000 to gadm28 areas for each polygon
+    # doesn't include anything to do with thresh - one to many
     join_field_list = ['iso', 'adm1', 'adm2']
-    first_merge = pd.merge(extent_df, adm2_area_df, how='left', on=join_field_list)
+    first_merge = pd.merge(extent_2000_df, adm2_area_df, how='left', on=join_field_list)
 
+    # now join to the areas of our AOIs - specific to polyname + bounds
     join_field_list.extend(['polyname', 'bound1', 'bound2', 'bound3', 'bound4'])
     second_merge = pd.merge(first_merge, poly_area_df, how='left', on=join_field_list)
 
     # gadm28 polygons don't have a default area_poly_aoi, so set it = gadm area
     second_merge.loc[second_merge.area_poly_aoi.isnull(), 'area_poly_aoi'] = second_merge['area_gadm28']
 
+    # gain data doesn't have thresh, but has unique values per poly AOI as well
     third_merge = pd.merge(second_merge, gain_df, how='left', on=join_field_list)
     third_merge.loc[third_merge.area_gain.isnull(), 'area_gain'] = 0
 
-    return third_merge
+    # now join to 2010 extent - add thresh to join list
+    join_field_list.extend(['thresh'])
+    fourth_merge = pd.merge(third_merge, extent_2010_df, how='left', on=join_field_list)
+
+    return fourth_merge
 
 
 def join_loss_extent(loss_df, extent_df, field_list):
@@ -91,13 +111,22 @@ def join_loss_extent(loss_df, extent_df, field_list):
     merged = pd.merge(extent_df, loss_df, how='left', on=join_list, suffixes=suffixes)
     
     # temporarily set nodata to -9999 - pandas can't group nd values
-    merged = merged.fillna(-9999)
+    bound_list = [x for x in merged.columns if 'bound' in x]
+    merged[bound_list] = merged[bound_list].fillna(-9999)
+
+    # for area values, set values to 0 where we have nodata
+    area_list = [x for x in merged.columns if 'area' in x]
+    merged[area_list] = merged[area_list].fillna(0)
+
+    # also update year to be -9999 if no loss data for this year
+    # we'll deal with these -9999 values for year later on
+    merged['year'] = merged.year.fillna(-9999)
 
     # explicitly set proper fieldnames to int
     for field_name in ['year', 'adm1', 'adm2']:
         merged[field_name] = merged[field_name].astype(int)
 
-    for field_name in ['bound1', 'bound2', 'bound3', 'bound4']:
+    for field_name in bound_list:
         merged[field_name] = merged[field_name].astype(unicode)
         
     # there are many adm2 areas with extent data but without loss
@@ -112,6 +141,8 @@ def join_loss_extent(loss_df, extent_df, field_list):
 
     # then update anything where year = -9999 to be the dummy_year value
     # and set loss and emissions for these instances to 0
+    print merged.head()
+    print merged.columns
     merged.loc[merged.year == -9999, ['area_loss', 'emissions']] = 0
     merged.loc[merged.year == -9999, ['year']] = merged['dummy_year']
 
