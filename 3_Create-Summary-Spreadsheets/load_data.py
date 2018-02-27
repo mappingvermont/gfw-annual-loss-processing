c035bc0f520d6f5000642418d8b4dac20b6290ba
mappingvermont <charlie.hofmann@gmail.com>
2018-02-27 21:40:06 +0000

    rework load data for new country-pages approach

--- 3_Create-Summary-Spreadsheets/load_data.py
+++ 3_Create-Summary-Spreadsheets/load_data.py
@@ -14,16 +14,18 @@ def main():
     root_dir = os.path.dirname(os.path.abspath(__file__))
     src_dir = os.path.join(root_dir, 'source')
 
-    extent2000_src = os.path.join(src_dir, 'extent2000.json')
-    loss_src = os.path.join(src_dir, 'loss.json')
+    extent2000_src = os.path.join(src_dir, 'extent2000.csv')
+    extent2010_src = os.path.join(src_dir, 'extent2010.csv')
+    loss_src = os.path.join(src_dir, 'loss.csv')
     gain_src = os.path.join(src_dir, 'gain.csv')
 
-    extent2000_df = json_to_df(extent2000_src)
-    loss_df = json_to_df(loss_src)
-    gain_df = pd.read_csv(gain_src, header=None, names=['iso', 'adm1', 'adm2', 'placeholder', 'area'])
+    extent2000_df = filter_csv(extent2000_src)
+    extent2010_df = filter_csv(extent2010_src)
+    loss_df = filter_csv(loss_src)
+    gain_df = filter_csv(gain_src)
 
-    table_list = ['extent', 'loss', 'gain']
-    df_list = [extent2000_df, loss_df, gain_df]
+    table_list = ['extent2000', 'extent2010', 'loss', 'gain']
+    df_list = [extent2000_df, extent2010_df, loss_df, gain_df]
 
     for table_name, df in zip(table_list, df_list):
         print 'inserting into {}'.format(table_name)
@@ -40,43 +42,59 @@ def main():
 
     lkp_df.to_sql('adm_lkp', conn, if_exists='replace')
 
-    for data_type in ['loss', 'extent', 'gain']:
-        add_missing_data(lkp_df, data_type)
+    for data_table in ['loss', 'extent2000', 'extent2010', 'gain']:
+        add_missing_data(lkp_df, data_table)
 
     print 'inserted'
 
 
-def json_to_df(json_file):
+def filter_csv(input_csv):
 
-    with open(json_file) as thefile:
-        data = json.load(thefile)['data']
+    print 'reading {}'.format(input_csv)
 
-    return pd.DataFrame(data)
+    # read in df, filtering for only gadm28
+    # don't care about any of the other datasets used by country pages
+    df = pd.read_csv(input_csv)
+    df = df[df.polyname == 'gadm28_large'].copy()
+
+    skip_col_list = ['polyname', 'bound1', 'bound2', 'bound3', 'bound4', 'emissions']
+    col_list = [x for x in df.columns.tolist() if x not in skip_col_list]
+
+    # remove unnecessary columns
+    df = df[col_list]
+
+    return df
 
 
-def add_missing_data(adm_df, data_type):
+def add_missing_data(adm_df, data_table):
 
-    sql = 'SELECT iso, adm1, adm2, sum(area) as area FROM {} GROUP BY iso, adm1, adm2'.format(data_type)        
+    # find existing iso/adm1/adm2 combinations in the data table of interest
+    sql = 'SELECT iso, adm1, adm2, sum(area) as area FROM {} GROUP BY iso, adm1, adm2'.format(data_table)        
     df = pd.read_sql(sql, conn)
 
+    # join them to the table of all iso/adm1/adm2
     join_field_list = ['iso', 'adm1', 'adm2']
     merged = pd.merge(adm_df, df, how='left', on=join_field_list)
 
+    # filter to get a list of iso/adm1/adm2 rows without any data in the source table
+    # source in this case being loss/extent2000/extent2010/gain
     missing_df = merged[merged.area.isnull()]        
     missing_df['area'] = 0.0
 
-    join_field_dict = {'extent': ['thresh'], 'loss': ['thresh', 'year']}
+    join_field_dict = {'extent2000': ['thresh'], 'extent2010': ['thresh'], 'loss': ['thresh', 'year']}
 
-    if data_type in ['extent', 'loss']:
-        join_field_list += join_field_dict[data_type]
+    if data_table in ['extent2000', 'extent2010', 'loss']:
+        join_field_list += join_field_dict[data_table]
 
         # create empty df
         dummy_df = pd.DataFrame()
 
+        # will need a thresh value for each missing iso/adm1/adm2 row
         for thresh in [0, 10, 15, 20, 25, 30, 50, 75]:
             missing_df['thresh'] = thresh
 
-            if data_type == 'loss':
+            # and if the input is loss, will need a year value as well
+            if data_table == 'loss':
                  for year in range(2001, 2017):
                      missing_df['year'] = year
 
@@ -93,7 +111,10 @@ def add_missing_data(adm_df, data_type):
     join_field_list += ['area']
     dummy_df = dummy_df[join_field_list]
 
-    dummy_df.to_sql(data_type, conn, if_exists='append', index=False)
+    # after we've built our dummy table, append it to the source table in sql
+    # this will add all the dummy rows, ensuring we have values for every iso/adm1/adm2 row possible
+    dummy_df.to_sql(data_table, conn, if_exists='append', index=False)
 
 if __name__ == '__main__':
     main()
+
